0f214360ed6768190d3f8c03fde71ad8e7dd0ecb
Keerthik <keerthiko@gmail.com>
2014-08-12 06:35:25 -0700

    source only

--- Legacy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Korcgames::Application.config.secret_token = '7a37272291d633230654269d8ee0c45d3a00a2c90299de0e77d6f51c73d938f7786967d21cb6ef81290bce237e20068375727f48a7637a258c9c052da5f9927e'
--- README.md
+++
@@ -1,7 +0,0 @@
-korcgames
-=========
-Source for personal home page: www.korc.me
-
-Mostly just HTML, CSS, the occasional static page generation script
-
-Made with Jekyll
