c2a7d3b9a60832108e536a1df16c8d8978642b8f
Keerthik <keerthiko@gmail.com>
2016-01-23 20:14:45 -0800

    much better animations, fully functional

--- js/hexes.js
+++ js/hexes.js
@@ -20,14 +20,16 @@ var RIGHT_KEY = 39;
 var UP_KEY = 38;
 var DOWN_KEY = 40;
 
-var cycle_classes = ["left_hex", "bottomleft_hex", "bottomright_hex", 
+var STATIC_CLASSES = ["left_hex", "bottomleft_hex", "bottomright_hex", 
 					 "right_hex", "topright_hex", "topleft_hex", 
 					 "center_hex"];
-var anim_classes = ["left_anim", "bottomleft_anim", "bottomright_anim", 
+var ANIM_CLASSES = ["left_anim", "bottomleft_anim", "bottomright_anim", 
 					 "right_anim", "topright_anim", "topleft_anim", 
 					 "center_anim"];
+var POP_CLASS = "pop_anim";
 
-var gameState = [6, 5, 4, 3, 2, 1, 0];
+var animating = false;
+var gameState = [6, 0, 0, 6, 0, 0, 0];
 var LEFTS = [0, 1, 5];
 var RIGHTS = [3, 2, 4];
 var CENTER = 6;
@@ -35,26 +37,36 @@ var CENTER = 6;
 var spawnTrigger = 4;
 
 function LaunchDance () {
-	$(".hexagon").each(function (index) {
+	var isFadeSet = false;
+	animating = true;
+	$('.hexagon.center_hex').each(function (index) {
 		if (index < 6)
-			$(this).removeClass("center_hex").addClass(anim_classes[index]);
+			$(this).removeClass('center_hex').addClass(ANIM_CLASSES[index]);
 		else
 			console.log("omg extra hexes: " + index);
-		/*
-		$(this).one(TRANSITION_END,
-		    function(e) {
-		    	var newIndex = Cycle(index, 1);
-		    	console.log($(this).attr('class') + " index: " + index + " " + newIndex);
-		    	$(this).removeClass(anim_classes[index]).addClass(anim_classes[newIndex]);
-		    	console.log($(this).attr('class') + " index: " + index + " " + newIndex);
-		    	$(this).one(TRANSITION_END, function (e) {
-					//console.log($(this).attr('class') + " index: " + newIndex);
-		    		$(this).removeClass(anim_classes[newIndex]).addClass(cycle_classes[newIndex]);
-		    	});
+		$(this).one(TRANSITION_END, function (e) {
+	    	var newIndex = Cycle(index, 1);
+	    	//console.log($(this).attr('class') + " index: " + index + " " + newIndex);
+	    	$(this).removeClass(ANIM_CLASSES[index]).addClass(ANIM_CLASSES[newIndex]);
+	    	//console.log($(this).attr('class') + " index: " + index + " " + newIndex);
+	    	/*
+	    	$(this).one(TRANSITION_END, function (e) {
+				//console.log($(this).attr('class') + " index: " + newIndex);
+	    		$(this).removeClass(ANIM_CLASSES[newIndex]).addClass(STATIC_CLASSES[newIndex]);
+	    	});
+			*/
+			if (isFadeSet) return;
+			isFadeSet = true;
+			$(this).one(TRANSITION_END, function (e) {
+				$('#static').removeClass('initial').addClass('ready');
+				$('#static').one(TRANSITION_END, function (e) {
+					$('#game_state').removeClass('initial').addClass('ready');
+					ReflectGameState();
+					animating = false;
+				});
+			});
 		});
-		*/
 	});
-
 }
 
 function Cycle(index, direction) {
@@ -68,10 +80,12 @@ function Cycle(index, direction) {
 function SlideIfPossible(iLHS, iRHS) {
 	if (gameState[iLHS] != 0 && 
 	       (gameState[iLHS] == gameState[iRHS] || gameState[iRHS] == 0)) {
+		var base_class = 'numbox ' + ((gameState[iRHS] > 0)?'hexagon_inner ':"");
 		gameState[iRHS] += gameState[iLHS];
 		gameState[iLHS] = 0;
 		// make a copy. animate it. destroy it.
-		$("#clone_" + iLHS).removeClass().addClass('clone ' + anim_classes[iRHS]);
+		$("#num_" + iLHS).removeClass().addClass('numbox hexagon_inner ' + ANIM_CLASSES[iRHS]);
+		$("#num_" + iRHS).addClass(base_class + POP_CLASS);
 		return 1;
 	}
 	return 0;
@@ -92,7 +106,10 @@ function UpdateGameWithInput (keyCode) {
 	var oldState = $.extend([], gameState);
 	var addNewNum = true;
 
-	//$('#anim_clones').css('display', 'block');
+	// We don't want to accept input while animating
+	if (animating) return;
+	animating = true;
+	//$('#anim_s').css('display', 'block');
 	//$('#game_state').css('display', 'none');
 	switch (keyCode) {
 		case LEFT_KEY: 
@@ -110,12 +127,11 @@ function UpdateGameWithInput (keyCode) {
 				}
 			}
 /*
-			$(".numbox.center_hex").removeClass("center_hex").addClass("left_hex");
-			$(".numbox.right_hex").addClass("center_hex");
-			$(".numbox.topright_hex").removeClass("topright_hex").addClass("topleft_hex");
-			$(".numbox.bottomright_hex").removeClass("bottomright_hex").addClass("bottomleft_hex");
+			$("..center_hex").removeClass("center_hex").addClass("left_hex");
+			$("..right_hex").addClass("center_hex");
+			$("..topright_hex").removeClass("topright_hex").addClass("topleft_hex");
+			$("..bottomright_hex").removeClass("bottomright_hex").addClass("bottomleft_hex");
 */			
-			console.log("slid left!");
 			break;
 
 		case RIGHT_KEY:
@@ -132,33 +148,33 @@ function UpdateGameWithInput (keyCode) {
 					changes += SlideIfPossible(LEFTS[0], CENTER);
 				}
 			}
-			console.log("slid right!");
 			break;
 
 		case UP_KEY:
-			var last = gameState[0];
-			for (var i = 0; i < 6; i++) {
-				gameState[i] = i==5?last:gameState[i+1];
+			var last = gameState[5];	
+			for (var i = 5; i > -1; i--) {
+				var base_class = 'numbox ' + ((gameState[i] > 0)?'hexagon_inner ':"");
+				gameState[i] = i==0?last:gameState[i-1];
 				var newIndex = Cycle(i, 1);
-				$("#clone_" + i).removeClass().addClass('clone ' + anim_classes[newIndex]);
+				$("#num_" + i).removeClass().addClass(base_class + ANIM_CLASSES[newIndex]);
 			}
 			changes ++;
-			console.log("rotated up!");
 			break;
 
 		case DOWN_KEY:
-			var last = gameState[5];	
-			for (var i = 5; i > -1; i--) {
-				gameState[i] = i==0?last:gameState[i-1];
+			var last = gameState[0];
+			for (var i = 0; i < 6; i++) {
+				var base_class = 'numbox ' + ((gameState[i] > 0)?'hexagon_inner ':"");
+				gameState[i] = i==5?last:gameState[i+1];
 				var newIndex = Cycle(i, -1);
-				$("#clone_" + i).removeClass().addClass('clone ' + anim_classes[newIndex]);
+				$("#num_" + i).removeClass().addClass(base_class + ANIM_CLASSES[newIndex]);
 			}
 			changes ++;
-			console.log("rotated down!");
 			break;
 
 		default:
-			//$('#anim_clones').css('display', 'none');
+			animating = false;
+			//$('#anim_s').css('display', 'none');
 			//$('#game_state').css('display', 'block');
 			break;
 	}
@@ -175,29 +191,14 @@ function UpdateGameWithInput (keyCode) {
 		case 96: spawnTrigger = 1; break;
 		case 768: spawnTrigger = 2; break;
 	}
-
-	ReflectGameState();
-
-    // Animation clones
-    $('.clone').bind(TRANSITION_END, function (e) {
-    	console.log("onendinglyfe");
-    	//$('#anim_clones').css('display', 'none');
-    	//$('#game_state').css('display', 'block');
-    	// Reset data
-    	var i = parseInt($(this).attr('id').match(/\d+/)[0]);
-    	$(this).html = (gameState[i] > 0)?gameState[i]:"";
-    	// Reset classes
-    	$(this).removeClass().addClass('clone ' + cycle_classes[i]);
-    });
-
 }
 
 function ReflectGameState() {
 	for (var i = 0; i < 7; i++) {
 		var inner = (gameState[i] > 0)?gameState[i]:"";
-		$("#num_" + i).html(inner);
-		$("#clone_" + i).html(inner);
-
+		$("#num_" + i).find('p').html(inner);
+		var base_class = 'numbox ' + ((gameState[i] > 0)?'hexagon_inner ':"");
+    	$("#num_" + i).removeClass().addClass(base_class + STATIC_CLASSES[i]);
 	}
 }
 
@@ -207,5 +208,18 @@ $(document).ready(function() {
 	// Controls
 	$('html').keydown(function(e) {
 		UpdateGameWithInput(e.which);
+    });
+    // Animation s
+    $('.numbox').bind(TRANSITION_END, function (e) {
+    	//$('#anim_s').css('display', 'none');
+    	//$('#game_state').css('display', 'block');
+    	// Reset data
+    	var i = parseInt($(this).attr('id').match(/\d+/)[0]);
+    	var inner = (gameState[i] > 0)?gameState[i]:"";
+    	$(this).find('p').html(inner);
+    	// Reset classes
+    	var base_class = 'numbox ' + ((gameState[i] > 0)?'hexagon_inner ':"");
+    	$(this).removeClass().addClass(base_class + STATIC_CLASSES[i]);
+    	animating = false;
     });
 });
