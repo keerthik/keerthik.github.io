3bbe1a9d01f863d26ddbf354d326e943c6d15bd3
Keerthik <keerthiko@gmail.com>
2017-05-03 16:27:56 -0700

    v0.91

--- js/dicestorm_units.js
+++ js/dicestorm_units.js
@@ -3,16 +3,18 @@ $(document).ready(function() {
 });
 
 function generateImages() {
+	$('.hexTop').addClass('printTop');
+	$('.hexBottom').addClass('printBottom');
 	var cards = $('.unit-card-outer');
-	//console.log(orbspecials);
 	console.log(cards);
 	for (var i = 0; i < cards.length; i++) {
-		
 		html2canvas(cards[i]).then(function(canvas) {
 			// Export to png's, possibly even append into pdf
 	    console.log(canvas);
 	    document.body.appendChild(canvas);
 		});
 	}	
+	// Hide the normal stuff
+	$('.faction-bloc').css('display','none');
 }
 
